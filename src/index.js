2e64d054e606dfe8dbeb5ed978a7fb309fca1e02
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-04-02 13:40:31 +0300

    final

--- src/index.js
+++ src/index.js
@@ -11,12 +11,21 @@ const button = document.querySelector(`.search-button`);
 const imageGallary = document.querySelector(`.gallery`);
 let searchImage = ``;
 let page = 1;
-
+let perPage = 40;
+//
 const loadMoreImg = async e => {
   e.preventDefault();
   page += 1;
   try {
     const result = await fetchImages(searchImage, page);
+    let totalPages = result.totalHits / perPage;
+    // searchImage = input.value.trim();
+    if (page >= totalPages) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      loadMoreBtn.disabled = true;
+    }
     imageGallary.insertAdjacentHTML(`beforeend`, renderImages(result.hits));
     lightbox.refresh();
   } catch (error) {
@@ -37,6 +46,7 @@ const searchImages = async evt => {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
+    loadMoreBtn.classList.add(`is-hidden`);
     return;
   }
   try {
@@ -49,6 +59,7 @@ const searchImages = async evt => {
       );
       return;
     }
+    Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
     imageGallary.insertAdjacentHTML(`beforeend`, renderImages(result.hits));
     lightbox.refresh();
     if (page === 1) {
